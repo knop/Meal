a93cd4f473a86d988ce738eb61530865f1fedecb
Xiaohui Chen <xh_chen@DST54920.local>
2013-09-06 15:23:59 +0800

    添加讯飞语音

--- src/com/team4/meal/MainActivity.java
+++ src/com/team4/meal/MainActivity.java
@@ -10,10 +10,18 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.iflytek.speech.ErrorCode;
+import com.iflytek.speech.ISpeechModule;
+import com.iflytek.speech.InitListener;
+import com.iflytek.speech.SpeechConstant;
+import com.iflytek.speech.SpeechSynthesizer;
+import com.iflytek.speech.SynthesizerListener;
 
 public class MainActivity extends Activity implements SensorEventListener{
 
@@ -22,14 +30,35 @@ public class MainActivity extends Activity implements SensorEventListener{
 	private TextView _textView;
 	private TextView _tvMenuName;
 	
+	private SpeechSynthesizer _iFlyTTS;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		_textView = (TextView)findViewById(R.id.text_view);
 		_tvMenuName = (TextView)findViewById(R.id.text_view_menu);
+		
+		_iFlyTTS = new SpeechSynthesizer(this, new InitListener() {
+
+			@Override
+			public void onInit(ISpeechModule arg0, int code) {
+				Log.d("[Xiaohui]", "InitListener init() code = " + code);
+	        	if (code == ErrorCode.SUCCESS) {
+//	        		((Button)findViewById(R.id.tts_play)).setEnabled(true);
+	        	}
+			}
+	    });
 	}
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        _iFlyTTS.stopSpeaking(mTtsListener);
+        // 退出时释放连接
+        _iFlyTTS.destory();
+    }
+	
 	@Override
 	protected void onPause() {
 		super.onPause();
@@ -84,8 +113,11 @@ public class MainActivity extends Activity implements SensorEventListener{
 	}
 	
 	private void updateMealWithIndex(int index) {
-		TFMeal meal = MealManager.instance().currentMenu().getMeals().get(index);
-		_textView.setText(meal.toString());
+		TFMenu menu = MealManager.instance().currentMenu();
+		TFMeal meal = menu.getMeals().get(index);
+		String text = meal.toString();
+		_textView.setText(text);
+		speak(menu.getName() + "去吃" + text + ",不准再摇!");
 	}
 	
 	@Override
@@ -105,4 +137,54 @@ public class MainActivity extends Activity implements SensorEventListener{
 			}
 		}
 	}
+	
+	private void speak(String text) {
+		_iFlyTTS.setParameter(SpeechConstant.ENGINE_TYPE, SpeechSynthesizer.TTS_ENGINE_TYPE_LOCAL);
+		_iFlyTTS.setParameter(SpeechSynthesizer.VOICE_NAME, SpeechSynthesizer.CLOUD_TTS_ROLE_XIAOYU);
+		_iFlyTTS.setParameter(SpeechSynthesizer.SPEED, "50");
+		_iFlyTTS.setParameter(SpeechSynthesizer.PITCH, "50");
+		int code = _iFlyTTS.startSpeaking(text, mTtsListener);
+		if(code != 0) {
+			Log.d("[Xiaohui]", "start speak error : " + code);
+		} else {
+			Log.d("[Xiaohui]", "start speak success.");
+		}
+	}
+	
+    /**
+     * 合成回调监听。
+     */
+
+    private SynthesizerListener mTtsListener = new SynthesizerListener.Stub() {
+    	
+        @Override
+        public void onBufferProgress(int progress) throws RemoteException {
+        	Log.d("[Xiaohui]", "onBufferProgress :" + progress);
+        }
+
+        @Override
+        public void onCompleted(int code) throws RemoteException {
+            Log.d("[Xiaohui]", "onCompleted code =" + code);
+        }
+
+        @Override
+        public void onSpeakBegin() throws RemoteException {
+            Log.d("[Xiaohui]", "onSpeakBegin");
+        }
+
+        @Override
+        public void onSpeakPaused() throws RemoteException {
+        	 Log.d("[Xiaohui]", "onSpeakPaused.");
+        }
+
+        @Override
+        public void onSpeakProgress(int progress) throws RemoteException {
+        	Log.d("[Xiaohui]", "onSpeakProgress :" + progress);
+        }
+
+        @Override
+        public void onSpeakResumed() throws RemoteException {
+        	Log.d("[Xiaohui]", "onSpeakResumed.");
+        }
+    };
 }
--- src/com/team4/meal/TFApplication.java
+++ src/com/team4/meal/TFApplication.java
@@ -2,13 +2,15 @@ package com.team4.meal;
 
 import android.app.Application;
 
+import com.iflytek.speech.SpeechUtility;
+
 public class TFApplication extends Application {
 
     @Override 
     public void onCreate() { 
         // TODO Auto-generated method stub 
         super.onCreate();
-        
+        SpeechUtility.getUtility(this).setAppid("5226b9ef");
         MealManager.instance().init(this);
     }    
     
